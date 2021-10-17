063c277eb93909ef8b7c105aee45604d73b9de72
Alexander Wang <zxcvb22217@gmail.com>
2021-10-17 23:27:56 +0800

    feat: setup a minimal UI without styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,67 @@
 import './App.module.scss'
+import { useState } from 'react'
+import { createShortUrl } from './apiClient'
+
+enum AppStatus {
+  EnteringUrl,
+  Pending,
+  ShowingShortUrl,
+  ShowingError,
+}
 
 function App() {
+  const [status, setStatus] = useState<AppStatus>(AppStatus.EnteringUrl)
+  const [shortUrl, setShortUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [longUrl, setLongUrl] = useState('')
+
   return (
     <div className="App">
-      <label>Enter a long URL</label>
-      <input type="url" />
-      <label>Short URL</label>
-      <input readOnly />
+      {status === AppStatus.EnteringUrl && (
+        <>
+          <label>Enter a long URL</label>
+          <input
+            value={longUrl}
+            type="url"
+            onChange={e => setLongUrl(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              createShortUrl(
+                longUrl,
+                shortUrl => {
+                  setShortUrl(shortUrl)
+                  setStatus(AppStatus.ShowingShortUrl)
+                },
+                errorMessage => {
+                  setErrorMessage(errorMessage)
+                  setStatus(AppStatus.ShowingError)
+                }
+              )
+              setStatus(AppStatus.Pending)
+            }}>
+            Shorten
+          </button>
+        </>
+      )}
+      {status === AppStatus.ShowingShortUrl && (
+        <>
+          <label>Short URL</label>
+          <input value={shortUrl} readOnly />
+          <button onClick={() => setStatus(AppStatus.EnteringUrl)}>
+            Shorten another
+          </button>
+        </>
+      )}
+      {status === AppStatus.Pending && <div>Shortening</div>}
+      {status === AppStatus.ShowingError && (
+        <>
+          <div>{errorMessage}</div>
+          <button onClick={() => setStatus(AppStatus.EnteringUrl)}>
+            Try again
+          </button>
+        </>
+      )}
     </div>
   )
 }
---
+++ frontend/src/apiClient.ts
@@ -0,0 +1,38 @@
+interface CreateShortUrlRequestData {
+  url: string
+}
+
+interface CreateShortUrlResponseData {
+  shortUrl: string
+}
+
+export async function createShortUrl(
+  url: string,
+  onSuccess: (shortUrl: string) => void,
+  onError: (message: string) => void
+) {
+  return fetch(`api/create`, {
+    method: 'POST',
+    body: JSON.stringify({ url } as CreateShortUrlRequestData),
+  })
+    .then(checkStatus)
+    .then(response => parseJson<CreateShortUrlResponseData>(response))
+    .then(data => {
+      onSuccess(data.shortUrl)
+    })
+    .catch((error: Error) => {
+      onError(error.message)
+    })
+}
+
+function checkStatus(response: Response) {
+  if (response.status >= 200 && response.status < 300) return response
+  const error = new Error(
+    `HTTP Error: ${response.status} ${response.statusText}`
+  )
+  throw error
+}
+
+function parseJson<T>(response: Response): Promise<T> {
+  return response.json()
+}
