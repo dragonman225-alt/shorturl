64c26b69f0ff9c15f279ff3656ff234d1aba715e
Alexander Wang <zxcvb22217@gmail.com>
2021-10-19 01:30:29 +0800

    chore: fix subtree force-push

--- create-dist/create-dist.sh
+++ create-dist/create-dist.sh
@@ -8,4 +8,10 @@ cp -r frontend/build dist/frontend
 cp -r backend/build dist/backend
 cp create-dist/package.json dist/package.json
 cp create-dist/ormconfig.json dist/ormconfig.json
-git subtree push --prefix dist origin deploy
+
+# Force-push subtree
+# https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
+
+git subtree split --prefix dist -b deploy # Create tmp local branch
+git push -f origin deploy:deploy # Force-push the branch
+git branch -D deploy # Delete tmp local branch
