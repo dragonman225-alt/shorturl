ad68b648e43b5aef465a2011fe3f45a47acee91b
Alexander Wang <zxcvb22217@gmail.com>
2021-10-19 16:59:10 +0800

    chore: simplify deployment process

--- create-dist/create-dist.sh
+++ create-dist/create-dist.sh
@@ -2,6 +2,17 @@
 
 # Run this script at the top level directory of the project.
 
+# Build frontend
+pushd frontend
+yarn build
+popd
+
+# Build backend
+pushd backend
+yarn build
+popd
+
+# Create dist
 rm -rf dist
 mkdir -p dist
 cp -r frontend/build dist/frontend
@@ -9,9 +20,12 @@ cp -r backend/build dist/backend
 cp create-dist/package.json dist/package.json
 cp create-dist/ormconfig.json dist/ormconfig.json
 
-# Force-push subtree
-# https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
+# Need to commit to current branch so that git subtree can detect changes
+git add dist
+git commit -m "chore: create dist"
 
+# Create (force-push) subtree and deploy
+# https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
 git subtree split --prefix dist -b deploy # Create tmp local branch
 git push -f origin deploy:deploy # Force-push the branch
 git branch -D deploy # Delete tmp local branch
